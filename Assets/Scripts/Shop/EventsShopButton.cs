8096258a150654a024d55ebf9a115d34a00771b3
JustTeRoR <pavlenok2000@gmail.com>
2021-12-14 22:11:13 +0300

    Refactored project a bit and corrected ui for level 2.

--- Assets/Scripts/Shop/EventsShopButton.cs
+++ Assets/Scripts/Shop/EventsShopButton.cs
@@ -2,18 +2,8 @@ using UnityEngine;
 using UnityEngine.EventSystems;
 
 public class EventsShopButton : MonoBehaviour {
-	/*
-	BuildManager buildManager;
-
-	void Awake()
-	{
-		buildManager = BuildManager.instance;
-	}
-*/
 	public void BuildTurret(TurretBlueprint turret)
 	{
-		// buildManager = BuildManager.instance;
 		BuildManager.instance.SelectTurretToBuild(turret);
-		// buildManager.SelectTurretToBuild(turret);
 	}
 }
--- Assets/Scripts/Shop/ShopPanel.cs
+++ Assets/Scripts/Shop/ShopPanel.cs
@@ -11,7 +11,7 @@ public class ShopPanel : MonoBehaviour
 
     private GameObject _panel;
     private EventsShopButton eventsShopButton = new EventsShopButton();
-    // private Turret[] _towers;
+   
     private BuildManager buildManager;
     public static ShopPanel Instance { get; private set; }
 
@@ -22,16 +22,13 @@ public class ShopPanel : MonoBehaviour
         _panel = gameObject;
         Instance = this;
         buildManager = BuildManager.instance;
-        // _towers = new Turret[towerPrefabs.Length];
-        // gameObject.AddComponent<BuilderController>();
     }
 
     private void Start()
     {
         for (int i = 0; i < towerPrefabs.Length; i++)
         {
-           // _towers[i] = towerPrefabs[i].GetComponent<Turret>();
-           CreateShopElement(towerPrefabs[i]);
+            CreateShopElement(towerPrefabs[i]);
         }
     }
 
@@ -41,11 +38,10 @@ public class ShopPanel : MonoBehaviour
             new GameObject("Turret", typeof(Image), typeof(Button), typeof(LayoutElement));
         newButton.transform.SetParent(_panel.transform);
         newButton.AddComponent<TurretPrefab>();
-        // newButton.GetComponent<TurretPrefab>().Turret = turret;
         newButton.GetComponent<Image>().sprite = turret.spriteTurret;
         RectTransform rc = newButton.GetComponent<RectTransform>();
        
-        rc.sizeDelta = new Vector2(5, 50);
+        rc.sizeDelta = new Vector2(3, 30);
         rc.localPosition = new Vector3(rc.localPosition.x, rc.localPosition.y, 2.2f);
         rc.localRotation = Quaternion.Euler(-3.5f, 0f, 0f);
         rc.localScale = new Vector3(25f, 3f, 3.5f);
@@ -54,21 +50,19 @@ public class ShopPanel : MonoBehaviour
         GameObject newText = new GameObject("Cost ", typeof(Text));
         newText.transform.SetParent(newButton.transform);
         rc = newText.GetComponent<RectTransform>();
-        rc.localPosition = new Vector3(0f, -19f, -1.5f);
-        rc.sizeDelta = new Vector2(11, 8.6f);
+        rc.localPosition = new Vector3(0.5f, -19f, -1.5f);
+        rc.sizeDelta = new Vector2(40, 20);
         rc.localRotation = Quaternion.Euler(0f, 0f, 0f);
-        rc.localScale = new Vector3(0.25f, 1, 1.1f);
+        rc.localScale = new Vector3(0.05f, 1, 1.1f);
 
 		// newText.AddComponent<Image>().image = (Sprite) Resources.Load("Sprites/coin");
          newText.GetComponent<Text>().text = turret.cost.ToString();
         newText.GetComponent<Text>().font = (Font) Resources.Load("Fonts/Roboto-Bold");
         newText.GetComponent<Text>().color = Color.yellow;
-        newText.GetComponent<Text>().fontSize = 6;
-        newText.GetComponent<Text>().alignment = TextAnchor.MiddleCenter;
+        newText.GetComponent<Text>().fontSize = 15;
+        newText.GetComponent<Text>().alignment = TextAnchor.MiddleLeft;
         
         
         newButton.GetComponent<Button>().onClick.AddListener(delegate { eventsShopButton.BuildTurret(turret); });
-		// TODO: adapt shop class for this
-        // newButton.AddComponent<EventsShopButton>();
     }
 }
